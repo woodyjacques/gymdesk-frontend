950c9239a43ab5da0cb9d3e08461c99f89ac752b
woodyjacques <woodyjacques1@gmail.com>
2025-10-21 18:07:44 -0400

    trabajando en el dise;o de inicio de sesion y register

--- main.js
+++ main.js
@@ -1,12 +1,73 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require("path");
+
+let registerModal = null;
+let loginModal = null;
 
 function createWindow() {
+  
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    autoHideMenuBar: true,
+    icon: path.join(__dirname, "src", "assets", "img", "logo.png"),
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
   })
 
   win.loadFile('index.html')
+
+  ipcMain.on('open-register-modal', () => {
+    if (loginModal && !loginModal.isDestroyed()) {
+      loginModal.close();
+    }
+    if (registerModal && !registerModal.isDestroyed()) {
+      registerModal.focus();
+      return;
+    }
+    registerModal = new BrowserWindow({
+      parent: win,
+      modal: true,
+      show: true,
+      width: 800,
+      height: 600,
+      autoHideMenuBar: true,
+      icon: path.join(__dirname, "src", "assets", "img", "logo.png"),
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+      }
+    });
+    registerModal.loadFile('src/html/register.html');
+    registerModal.on('closed', () => { registerModal = null; });
+  });
+
+  ipcMain.on('open-login-modal', () => {
+    if (registerModal && !registerModal.isDestroyed()) {
+      registerModal.close();
+    }
+    if (loginModal && !loginModal.isDestroyed()) {
+      loginModal.focus();
+      return;
+    }
+    loginModal = new BrowserWindow({
+      parent: win,
+      modal: true,
+      show: true,
+      width: 800,
+      height: 600,
+      autoHideMenuBar: true,
+      icon: path.join(__dirname, "src", "assets", "img", "logo.png"),
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+      }
+    });
+    loginModal.loadFile('index.html');
+    loginModal.on('closed', () => { loginModal = null; });
+  });
 }
 
 app.whenReady().then(createWindow)
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,html}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      spaceGrotesk: ['"Space Grotesk"', 'sans-serif'],
+    }
+  },
+  plugins: [],
+}
